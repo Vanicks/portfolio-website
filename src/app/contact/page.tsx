90438f398ce8a6d9e981cd42396cabebc35c51e6
NotKaskus <andrewonrubia@gmail.com>
2024-01-23 17:19:52 +0800

    Adjust minimum height of the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,68 +8,7 @@ import Contact from '@/components/Contact';
 
 export default function page() {
   return (
-    // <main className='flex justify-center w-full my-7xl lg:my-[4%]'>
-    //   <div className='flex flex-col gap-8 mx-auto max-w-7xl w-full px-8 md:px-10 mt-5'>
-    //     <div className="grid sm:grid-cols-2 items-center gap-16 my-6 mx-auto max-w-4xl text-[#333] font-[sans-serif]">
-    //       <div>
-    //         <h1 className="text-3xl text-primary font-extrabold">Let's Talk</h1>
-    //         <p className="text-sm text-muted-foreground mt-3">Have some big idea or brand to develop and need help? Then reach out we'd love to hear about your project  and provide help.</p>
-    //         <div className="mt-12">
-    //             <h2 className="text-lg text-primary font-extrabold">Email</h2>
-    //             <ul className="mt-3">
-    //                 <li className="flex items-center">
-    //                     <div className="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-    //                         <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#007bff'
-    //                             viewBox="0 0 479.058 479.058">
-    //                             <path
-    //                                 d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-    //                                 data-original="#000000" />
-    //                         </svg>
-    //                     </div>
-    //                     <a href="/test" className="text-[#007bff] text-sm ml-3">
-    //                         <small className="block">Mail</small>
-    //                         <strong>info@example.com</strong>
-    //                     </a>
-    //                 </li>
-    //             </ul>
-    //         </div>
-    //         <div className="mt-12">
-    //             <h2 className="text-lg text-primary font-extrabold">Socials</h2>
-    //             <ul className="flex mt-3 space-x-4">
-    //                 <li className="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-    //                     <a href="/test">
-    //                         <FaGithub />
-    //                     </a>
-    //                 </li>
-    //                 <li className="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-    //                     <a href="/test">
-    //                         <FaInstagram />
-    //                     </a>
-    //                 </li>
-    //                 <li className="bg-[#e6e6e6cf] h-10 w-10 rounded-full flex items-center justify-center shrink-0">
-    //                     <a href="/test">
-    //                         <FaTwitter />
-    //                     </a>
-    //                 </li>
-    //             </ul>
-    //         </div>
-    //       </div>
-    //       <form className="ml-auo space-y-4">
-    //           <input type='text' placeholder='Name'
-    //               className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]" />
-    //           <input type='email' placeholder='Email'
-    //               className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]" />
-    //           <input type='text' placeholder='Subject'
-    //               className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]" />
-    //           <textarea placeholder='Message' rows={6}
-    //               className="w-full rounded-md px-4 bg-gray-100 text-sm pt-3 outline-[#007bff]"></textarea>
-    //           <button type='button'
-    //               className="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-3 w-full">Send</button>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </main>
-    <main className='p-6'>
+    <main className='p-6 min-h-screen mb-[10px]'>
       <h2 className="mb-2 scroll-mt-20 box-decoration-clone bg-clip-text text-[1.7rem] font-[750] motion-reduce:transition-none mt-6 text-center text-4xl">
           Contact me<span className="bg-gradient-to-r from-[#6310ff] to-[#1491ff] box-decoration-clone bg-clip-text text-fill-transparent dark:from-[#a2facf] dark:to-[#64acff]">.</span>
       </h2>
