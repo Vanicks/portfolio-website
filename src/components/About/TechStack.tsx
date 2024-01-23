29b7ee7e874e41f0fdfc6ca74cf827b5e61ce585
NotKaskus <andrewonrubia@gmail.com>
2024-01-23 17:31:30 +0800

    Update text formating for techstack component

--- src/components/About/TechStack.tsx
+++ src/components/About/TechStack.tsx
@@ -7,8 +7,8 @@ export default function TechStack() {
   return (
     <>
       <div className='mb-7'>
-        <h2 className="text-primary text-2xl lg:text-3xl font-semibold flex gap-2 items-center mb-2">Technologies</h2>
-        <p className='text-muted-foreground text-leading-6 text-lg tracking-wide'>
+        <h2 className="text-3xl font-bold tracking-tight text-primary md:text-5xl flex gap-2 items-center mb-2">Technologies</h2>
+        <p className='text-[#1e2024] dark:text-[#dadde7] text-leading-6 text-lg tracking-wide'>
           I use a variety of tools to streamline my development process and increase the quality 
           of both my code, and my projects. Below is a list of technologies and languages I've 
           had experience with in the past, or use currently.
