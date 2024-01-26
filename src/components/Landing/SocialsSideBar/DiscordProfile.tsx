ce4a6a41e08509a40746d2640bde7c50ae309f53
Paul Andrew <75168528+NotKaskus@users.noreply.github.com>
2024-01-26 08:29:51 +0000

    Update DiscordProfile component with Image component

--- src/components/Landing/SocialsSideBar/DiscordProfile.tsx
+++ src/components/Landing/SocialsSideBar/DiscordProfile.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { Suspense } from 'react';
+import Image from 'next/image';
 import { Skeleton } from '../../ui/skeleton'
 
 function LoadingSkeleton() {
   return (
     <>
-      <Skeleton className='h-60 w-60 bg-black rounded-md'></Skeleton>
+      <Skeleton className='h-400 w-200 bg-black rounded-md'></Skeleton>
     </>
   )
 }
@@ -14,10 +14,12 @@ function LoadingSkeleton() {
 export default function DiscordProfile() {
   return (
     <div className='flex flex-col bg-red'>
-      <img 
+      <Image 
         alt="My shiny discord profile card"
         src='https://lanyard.kyrie25.me/api/738748102311280681?imgStyle=square&bg=#292b2f&gradient=645CBB-A084DC-BFACE2-EBC7E6'
         loading='lazy'
+        width={400}
+        height={200}
       />
     </div>
   )
