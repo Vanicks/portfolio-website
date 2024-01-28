8fe8ed14849af7fcd46b00ff299d5f6b23cfef35
Paul Andrew <75168528+NotKaskus@users.noreply.github.com>
2024-01-28 06:18:10 +0000

    Add ESLint configuration file

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['db-types.ts'],
+  rules: {
+    semi: 'warn',
+    eqeqeq: 'warn',
+    curly: 'warn',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
+};
+
+module.exports = config;
