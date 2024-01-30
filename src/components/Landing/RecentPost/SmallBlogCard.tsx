b94708671adaaee433cb9bd0f063d3d623bc938a
Paul Andrew <75168528+NotKaskus@users.noreply.github.com>
2024-01-30 07:22:50 +0000

    Update bottom border style in SmallPostCard and SmallBlogCard

--- src/components/Landing/RecentPost/SmallBlogCard.tsx
+++ src/components/Landing/RecentPost/SmallBlogCard.tsx
@@ -41,7 +41,7 @@ function SmallBlogCard({ data, isNew }: CardOptions) {
               Posted {formattedDistance}
             </time>
           </div>
-          <hr className="w-full article-bottom-border h-[1px]" />
+          <hr className="w-full border-none bg-gradient-to-r from-[#b3b7bb] dark:from-[#50535e] to-transparent h-[1px]" />
           <ul className="flex px-4 py-2 gap-2 flex-wrap text-xs text-primary">
             <li className="text-primary">{readingTimeText}</li>
             <li className="h-full w-[1px] mr-1">
