af5c7fc861e98f9f1496f153c011acfa797b3d73
Paul Andrew <75168528+NotKaskus@users.noreply.github.com>
2024-01-31 06:18:24 +0000

    Remove @vercel/speed-insights package

--- src/components/AnalyticsWrapper.tsx
+++ src/components/AnalyticsWrapper.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function AnalyticsWrapper() {
   if (process.env.NODE_ENV !== 'production') {
@@ -11,7 +8,6 @@ export default function AnalyticsWrapper() {
   return (
     <>
       <Analytics />
-      <SpeedInsights />
     </>
   );
 }
